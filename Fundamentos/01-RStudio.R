6ffd7603892696c350f51564a0a479bce751ccc9
Lucas Vinicius <lucasvinij@gmail.com>
2022-10-08 14:11:14 -0300

    Add files R

---
+++ Fundamentos/01-RStudio.R
@@ -0,0 +1,68 @@
+# Explorando o RStudio
+
+# Nome dos Contributors
+contributors()
+
+
+# Licença
+license()
+
+
+# Configurando o diretório de trabalho
+getwd()
+setwd(‘C:/RFundamentos/Parte1’)
+
+
+# Informações sobre a sessão
+sessionInfo()
+
+
+# Imprimir na tela
+print('R - Uma das principais ferramentas do Cientista de Dados')
+
+
+# Criar gráficos
+plot(1:30)
+hist(rnorm(10))
+
+
+# Imprimir na tela
+print('R - Uma das principais ferramentas do Cientista de Dados')
+
+
+# Instalar pacotes
+install.packages('randomForest')
+install.packages('ggplot2')
+install.packages("dplyr")
+
+
+# Carregar o pacote
+library(ggplot2)
+
+
+# Descarregar o pacote
+detach(package:ggplot2)
+
+
+
+# Visualizando o diretório de trabalho
+getwd()
+
+
+# Se souber o nome da função
+help(mean)
+?mean
+
+
+# Se não souber o nome da função
+help.search('randomForest')
+help.search('matplot')
+??matplot
+RSiteSearch('matplot')
+example('matplot')
+
+
+# Sair
+q()
+
+
